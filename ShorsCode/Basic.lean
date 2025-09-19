e4ae9c12c9ae72c1786520ba9828569e66f67941
Stavan Jain <stavansjain@gmail.com>
2025-09-19 17:04:58 -0500

    Initial Commit

--- ShorsCode/Basic.lean
+++ ShorsCode/Basic.lean
@@ -1,1 +1,58 @@
-def hello := "world"
+import Mathlib.LinearAlgebra.Matrix.Defs
+import Mathlib.LinearAlgebra.Matrix.Notation
+import Mathlib.Data.Complex.Basic
+import Mathlib.Data.Matrix.Mul
+
+open Matrix Complex
+
+abbrev QuantumGate := Matrix (Fin 2) (Fin 2) ℂ
+abbrev Qubit := Matrix (Fin 2) (Fin 1) ℂ
+abbrev i := I
+
+def QuantumWire : QuantumGate := !![1,0;0,1]
+def X : QuantumGate := !![0,1;1,0]
+def Z : QuantumGate := !![1,0;0,-1]
+def Y : QuantumGate := !![0, -i; i, 0]
+
+def Involutary (X : QuantumGate) : Prop := X * X = QuantumWire
+
+def ket0 : Qubit := !![1 ; 0]
+def ket1 : Qubit := !![0 ; 1]
+
+def applyGate (U : QuantumGate) (ψ : Qubit) := U * ψ
+
+@[simp] lemma X_def : X = !![0, 1; 1, 0] := rfl
+@[simp] lemma Z_def : Z = !![1, 0; 0, -1] := rfl
+@[simp] lemma Y_def : Y = !![0, -i; i, 0] := rfl
+@[simp] lemma QuantumWire_def : QuantumWire = !![1, 0; 0, 1] := rfl
+@[simp] lemma applyGate_def {U : QuantumGate} {ψ : Qubit} : applyGate U ψ  = U * ψ := rfl
+
+lemma X_on_ket0 : applyGate X ket0 = ket1 := by
+  ext i j
+  simp [ket0, ket1]
+
+lemma X_on_ket1 : applyGate X ket1 = ket0 := by
+  ext i j
+  simp [X, ket0, ket1]
+
+lemma X_involutary : Involutary X := by
+  ext i j
+  simp
+
+lemma Z_involutary : Involutary Z := by
+  ext i j
+  simp
+
+lemma Y_involutary : Involutary Y := by
+  ext i j
+  simp
+
+example : Matrix.map X id  = X := by
+  ext i j
+  apply map_apply
+
+lemma XZ_neg_ZX : X * Z = - (Z * X) := by
+  ext i j
+  fin_cases i <;> fin_cases j
+  <;> rw [Matrix.mul_apply]
+  <;> simp
---
+++ ShorsCode/Basic2.lean
@@ -0,0 +1,144 @@
+import Mathlib.LinearAlgebra.Matrix.Defs
+import Mathlib.LinearAlgebra.Matrix.Notation
+import Mathlib.Data.Complex.Basic
+import Mathlib.Data.Matrix.Mul
+import Mathlib.Analysis.Matrix
+import Mathlib.Tactic
+
+open Matrix Complex
+open scoped BigOperators ComplexConjugate
+
+abbrev QubitVec := Fin 2 → ℂ
+
+noncomputable def l2norm (v : QubitVec) : ℝ :=
+  ‖v 0‖^2 + ‖v 1‖^2
+
+@[simp] lemma l2norm_def {v : QubitVec} : l2norm v = ‖v 0‖^2 + ‖v 1‖^2 := rfl
+
+structure Qubit where
+  state : QubitVec
+  normalized : l2norm state = 1
+
+def ket0 : Qubit :=
+  { state := ![1, 0],
+    normalized := by simp }
+
+def ket1 : Qubit :=
+  { state := ![0, 1],
+    normalized := by simp}
+
+def Unitary (U : Matrix (Fin 2) (Fin 2) ℂ) : Prop :=
+  (U * Uᴴ = 1) ∧ (Uᴴ * U = 1)
+
+structure QuantumGate where
+  U : Matrix (Fin 2) (Fin 2) ℂ
+  unitary : Unitary U
+
+lemma l2normQubitState (ψ : Qubit) : l2norm ψ.state = 1 := ψ.normalized
+
+noncomputable def applyMatrixVec (U : Matrix (Fin 2) (Fin 2) ℂ) (ψ : QubitVec):=
+fun i => ∑ j, U i j * ψ j
+
+lemma l2norm_unitary
+    {U : Matrix (Fin 2) (Fin 2) ℂ}
+    (hU : Unitary U) :
+    ∀ v : QubitVec, l2norm (applyMatrixVec U v) = l2norm v := sorry
+
+abbrev i := I
+
+noncomputable def applyGate (G : QuantumGate) (ψ : Qubit) : Qubit :=
+  { state := applyMatrixVec G.U ψ.state,
+    normalized := by
+      have := l2norm_unitary G.unitary ψ.state
+      rw [← l2normQubitState ψ]
+      exact this
+  }
+
+@[simp] def Hermitian {n : ℕ} (M : Matrix (Fin n) (Fin n) ℂ) : Prop := Mᴴ = M
+
+@[simp] def Involutary {n : ℕ} (M : Matrix (Fin n) (Fin n) ℂ) : Prop := M * M = 1
+
+/-- If a matrix is Hermitian and involutary, then it is unitary. -/
+lemma unitary_of_hermitian_involutary
+  {n : ℕ} {U : Matrix (Fin n) (Fin n) ℂ}
+  (hH : Hermitian U) (hI : Involutary U) :
+  (U * Uᴴ = 1) ∧ (Uᴴ * U = 1) := by
+  -- hH : Uᴴ = U, hI : U * U = 1
+  have hH' : Uᴴ = U := hH
+  refine ⟨?left, ?right⟩
+  · -- U * Uᴴ = 1
+    simpa [hH'] using hI
+  · -- Uᴴ * U = 1
+    simpa [hH'] using hI
+
+/- Defining the Identity Quantum Gate -/
+lemma id_Hermitian : Hermitian (1 : Matrix (Fin 2) (Fin 2) ℂ) := by simp
+
+lemma id_Involutary : Involutary (1 : Matrix (Fin 2) (Fin 2) ℂ) := by simp
+
+/-- Identity matrix packaged as a `QuantumGate`. -/
+def Igate : QuantumGate :=
+  { U := 1
+  , unitary := unitary_of_hermitian_involutary id_Hermitian id_Involutary
+  }
+
+/- Creating the Pauli X Quantum Gate -/
+def Xmat : Matrix (Fin 2) (Fin 2) ℂ := !![0, 1; 1, 0]
+lemma Xmat_hermitian : Hermitian Xmat := by
+  ext i j
+  fin_cases i <;> fin_cases j <;> simp [Xmat]
+
+lemma Xmat_involutary : Involutary Xmat := by
+  ext i j
+  fin_cases i <;> fin_cases j <;>
+    simp [Xmat, Matrix.mul_apply]
+
+/-- Pauli X packaged as a `QuantumGate`. -/
+def Xgate : QuantumGate :=
+  { U := Xmat
+  , unitary := unitary_of_hermitian_involutary Xmat_hermitian Xmat_involutary
+  }
+
+/- Creating the Pauli Y Quantum Gate -/
+def Ymat : Matrix (Fin 2) (Fin 2) ℂ := !![0, -i; i, 0]
+
+lemma Ymat_hermitian : Hermitian Ymat := by
+  ext i j
+  fin_cases i <;> fin_cases j <;> simp [Ymat]
+
+lemma Ymat_involutary : Involutary Ymat := by
+  ext i j
+  fin_cases i <;> fin_cases j <;>
+    simp [Ymat, Matrix.mul_apply]
+
+/-- Pauli Y packaged as a `QuantumGate`. -/
+def Ygate : QuantumGate :=
+  { U := Ymat
+  , unitary := unitary_of_hermitian_involutary Ymat_hermitian Ymat_involutary
+  }
+
+/- Creating the Pauli Z Quantum Gate -/
+def Zmat : Matrix (Fin 2) (Fin 2) ℂ := !![1, 0; 0, -1]
+lemma Zmat_hermitian : Hermitian Zmat := by
+  ext i j
+  fin_cases i <;> fin_cases j <;> simp [Zmat]
+
+lemma Zmat_involutary : Involutary Zmat := by
+  ext i j
+  fin_cases i <;> fin_cases j <;>
+    simp [Zmat, Matrix.mul_apply]
+
+/-- Pauli Y packaged as a `QuantumGate`. -/
+def Zgate : QuantumGate :=
+  { U := Zmat
+  , unitary := unitary_of_hermitian_involutary Zmat_hermitian Zmat_involutary
+  }
+
+lemma X_on_ket0 : applyGate Xgate ket0 = ket1 := by admit
+
+lemma X_on_ket1 : applyGate Xgate ket1 = ket0 := by admit
+
+-- TODO: try to get simp to auto-apply everything
+lemma XZ_neg_ZX : Xgate.U * Zgate.U = - (Zgate.U * Xgate.U) := by
+  ext i j
+  fin_cases i <;> fin_cases j <;> simp [Xgate, Xmat, Zgate, Zmat]
